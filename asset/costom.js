82556e788a338e06d7f53c34870cf5d7073b4787
abuhenashoaib <mdshoaib87392@gmsil.com>
2024-11-17 21:25:28 +0600

    exam

---
+++ asset/costom.js
@@ -0,0 +1,83 @@
+// Question-01
+
+let name ='john'
+    age ='25';
+
+    console.log('My name is john and I am 25 years old');
+
+    // Question-02
+
+    let num = [9];
+    if (num % 2) {
+        console.log('This number is even');
+    } else {
+        console.log('This number is odd');
+        
+    }
+    
+    // Question-03
+    
+    for (let i = 0; i <= 6; i++){
+        console.log(i);
+        
+    }
+
+    // Question-04
+
+    function addNumbers(a, b) {
+        return a + b;
+    }
+    let outfut = addNumbers (8, 9);
+    console.log(outfut);
+
+    // Question-05
+
+    const fruits = ["apple", "banana", "cherry"];
+    fruits.push ("orange");
+    console.log(fruits [0]);
+    console.log(fruits.length);
+
+
+    // Question-06
+
+    const numbers = [1, 2, 3, 4, 5]
+    numbers.forEach((number) => {
+        console.log(number * 2);
+
+    })
+
+
+    // Question-07
+
+
+
+
+    // Question-08
+
+    let text = "hello world";
+    console.log(text.toUpperCase ());
+
+    // Question-09
+
+    const person = {
+        name:"Shoaib",
+        age:25,
+
+        greet:function (){
+            return  "Hello, my name is Shoaib";
+
+        }
+    }
+    console.log(person.greet());
+   
+    
+    
+
+
+       
+    
+    
+    
+   
+        
+    
